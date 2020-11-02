e339a30626e351eb7c82cc44a1fa1eee7bf546de
wslgs <weslley_45@eng.ci.ufpb.br>
2020-11-02 00:30:49 -0300

    Board exception and defensive programming

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package Chess;
 
+import Chess.pieces.Rook;
 import boardgame.Board;
+import boardgame.Position;
 
 public class ChessMatch {
 	
@@ -8,6 +10,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -18,6 +21,10 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
 	}
 	
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -5,23 +5,21 @@ import Chess.ChessPiece;
 public class UI {
 
 	public static void printBoard(ChessPiece[][] pieces) {
-		System.out.println();
 		for(int i=0; i<pieces.length; i++) {
-			System.out.print((8 - i) + "   ");
+			System.out.print((8 - i) + " ");
 			for(int j=0; j<pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println();
-		System.out.println("    A B C D E F G H");
+		System.out.println("  A B C D E F G H");
 	}
 	
 	private static void printPiece(ChessPiece piece) {
 		if(piece == null)
 			System.out.print("-");
 		else
-			System.out.println(piece);
+			System.out.print(piece);
 		System.out.print(" ");
 	}
 	
